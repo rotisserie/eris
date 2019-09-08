c14533c8bc0c406318ee97f0c1de35cb005f91a2
Chris Harless <53271741+morningvera@users.noreply.github.com>
2019-09-08 13:24:03 -0400

    Improve error wrapping test

--- error_test.go
+++ error_test.go
@@ -6,8 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestError(t *testing.T) {
-	err := New("test error")
-	wrapErr := Wrap(err, "additional context")
-	assert.Equal(t, wrapErr.Error(), "additional context: test error")
+func TestErrorWrapping(t *testing.T) {
+	tests := map[string]struct {
+		wrapStrs []string // input error strings
+		errStr   string   // expected output string
+	}{
+		"standard error wrapping": {
+			wrapStrs: []string{"test error", "additional context", "even more context"},
+			errStr:   "even more context: additional context: test error",
+		},
+	}
+	for desc, tc := range tests {
+		tc := tc
+		t.Run(desc, func(t *testing.T) {
+			t.Parallel()
+			var err error
+			for i, str := range tc.wrapStrs {
+				if i == 0 {
+					err = New(str)
+				} else {
+					err = Wrap(err, str)
+				}
+			}
+			assert.Equalf(t, tc.errStr, err.Error(), "%v: expected { %v } got { %v }", desc, tc.errStr, err)
+		})
+	}
 }
