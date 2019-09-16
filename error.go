78e682b75b91d62aa4510a0e0832d79e0716d3e2
Chris Harless <53271741+morningvera@users.noreply.github.com>
2019-09-16 10:26:29 -0400

    feat: add stack trace implementation and root error types
    also modifies the wrap/f methods to correctly handle all error types

--- error.go
+++ error.go
@@ -2,56 +2,114 @@ package eris
 
 import (
 	"fmt"
-
-	"golang.org/x/xerrors"
+	"io"
 )
 
+// New creates a new root error.
 func New(msg string) error {
-	return &errorStr{
+	return &rootError{
 		msg:   msg,
-		frame: xerrors.Caller(1),
+		stack: callers(),
 	}
 }
 
+// Errorf create a new root error.
+func Errorf(format string, args ...interface{}) error {
+	return &rootError{
+		msg:   fmt.Sprintf(format, args...),
+		stack: callers(),
+	}
+}
+
+// Wrapf adds additional context to all error types. For root error types, this method resets the stack
+// trace, which solves the problem of incorrect stack traces when using global/sentinel error values. For
+// wrapped error types, this method simply wraps the error with the new context. For all other error types,
+// it returns a new error with a correct stack trace.
 func Wrap(err error, msg string) error {
-	return &errorStr{
-		msg:   msg,
-		err:   err,
-		frame: xerrors.Caller(1),
+	if root, ok := err.(*rootError); ok {
+		root.stack = callers()
+		return &wrapError{
+			msg:   msg,
+			err:   root,
+			frame: caller(),
+		}
+	}
+	if wrap, ok := err.(*wrapError); ok {
+		return &wrapError{
+			msg:   msg,
+			err:   wrap,
+			frame: caller(),
+		}
+	}
+	return &rootError{
+		msg:   err.Error(),
+		stack: callers(),
 	}
 }
 
+// Wrapf adds additional context to all error types. For root error types, this method resets the stack
+// trace, which solves the problem of incorrect stack traces when using global/sentinel error values. For
+// wrapped error types, this method simply wraps the error with the new context. For all other error types,
+// it returns a new error with a correct stack trace.
 func Wrapf(err error, format string, args ...interface{}) error {
 	msg := fmt.Sprintf(format, args...)
-	return &errorStr{
-		msg:   msg,
-		err:   err,
-		frame: xerrors.Caller(1),
+	if root, ok := err.(*rootError); ok {
+		root.stack = callers()
+		return &wrapError{
+			msg:   msg,
+			err:   root,
+			frame: caller(),
+		}
+	}
+	if wrap, ok := err.(*wrapError); ok {
+		return &wrapError{
+			msg:   msg,
+			err:   wrap,
+			frame: caller(),
+		}
+	}
+	return &rootError{
+		msg:   err.Error(),
+		stack: callers(),
 	}
 }
 
-type errorStr struct {
+type rootError struct {
 	msg   string
-	err   error
-	frame xerrors.Frame
+	stack *stack // todo: should this be a pointer?
 }
 
-func (e errorStr) Error() string {
+func (e *rootError) Error() string {
 	return fmt.Sprint(e)
 }
 
-func (e errorStr) Format(f fmt.State, c rune) {
-	xerrors.FormatError(e, f, c)
-}
-
-func (e errorStr) FormatError(p xerrors.Printer) error {
-	p.Print(e.msg)
-	if p.Detail() {
-		e.frame.Format(p)
+// todo: this is pretty rough right now (use the DefaultFormatter to print)
+func (e *rootError) Format(s fmt.State, verb rune) {
+	io.WriteString(s, e.msg)
+	for _, f := range *e.stack { // todo: check nil?
+		frame := frame(f)
+		io.WriteString(s, "\n\t")
+		io.WriteString(s, fmt.Sprintf("%v | %v:%v", frame.file(), frame.name(), frame.line()))
 	}
-	return e.err
 }
 
-func (e errorStr) Unwrap() error {
+type wrapError struct {
+	msg   string
+	err   error
+	frame *frame
+}
+
+func (e wrapError) Error() string {
+	return fmt.Sprint(e)
+}
+
+func (e *wrapError) Format(s fmt.State, verb rune) {
+	io.WriteString(s, e.msg)
+	io.WriteString(s, " (")
+	io.WriteString(s, fmt.Sprintf("%v | %v:%v", e.frame.file(), e.frame.name(), e.frame.line()))
+	io.WriteString(s, ")")
+}
+
+func (e *wrapError) Unwrap() error {
 	return e.err
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,1 @@
 module github.com/morningvera/eris
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.2.2
-	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
-)
