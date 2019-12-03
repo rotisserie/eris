a9394137cae61073c0a7f85397c0e6dd2d3af644
Chris Harless <53271741+morningvera@users.noreply.github.com>
2019-12-03 15:45:28 -0500

    feat: add error type checking

--- eris.go
+++ eris.go
@@ -4,6 +4,7 @@ package eris
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // New creates a new root error with a static message.
@@ -39,13 +40,6 @@ func Wrapf(err error, format string, args ...interface{}) error {
 	return wrap(err, fmt.Sprintf(format, args...))
 }
 
-// todo: fix callers interface to allow passing the number to skip
-//       this will allow callers to work from helper methods
-//       pretty sure callers(4) is appropriate for this case and callers(3) is appropriate for New/Errorf
-//       is this actually necessary?
-// todo: test what happens to global error stack traces when this is called
-//       does pointer cause global stack to be reset?
-//       seems like it modifies the input error because of pointers, is this desirable?
 func wrap(err error, msg string) error {
 	if err == nil {
 		return nil
@@ -66,11 +60,95 @@ func wrap(err error, msg string) error {
 	}
 }
 
-// todo: func Is(err, target error) bool
+// Unwrap returns the result of calling the Unwrap method on err, if err's type contains an Unwrap method
+// returning error. Otherwise, Unwrap returns nil.
+func Unwrap(err error) error {
+	u, ok := err.(interface {
+		Unwrap() error
+	})
+	if !ok {
+		return nil
+	}
+	return u.Unwrap()
+}
 
-// todo: func Cause(err error) error
+// Is reports whether any error in err's chain matches target.
+//
+// The chain consists of err itself followed by the sequence of errors obtained by repeatedly calling Unwrap.
+//
+// An error is considered to match a target if it is equal to that target or if it implements a method
+// Is(error) bool such that Is(target) returns true.
+func Is(err, target error) bool {
+	if target == nil {
+		return err == target
+	}
 
-// todo: should this also have a single frame?
+	isComparable := reflect.TypeOf(target).Comparable()
+	for {
+		if isComparable && err == target {
+			return true
+		}
+		if x, ok := err.(interface{ Is(error) bool }); ok && x.Is(target) {
+			return true
+		}
+		// TODO: consider supporing target.Is(err). This would allow
+		// user-definable predicates, but also may allow for coping with sloppy
+		// APIs, thereby making it easier to get away with them.
+		if err = Unwrap(err); err == nil {
+			return false
+		}
+	}
+}
+
+// As finds the first error in err's chain that matches target, and if so, sets target to that error value
+// and returns true.
+//
+// The chain consists of err itself followed by the sequence of errors obtained by repeatedly calling Unwrap.
+//
+// An error matches target if the error's concrete value is assignable to the value pointed to by target, or
+// if the error has a method As(interface{}) bool such that As(target) returns true. In the latter case, the
+// As method is responsible for setting target.
+//
+// As will panic if target is not a non-nil pointer to either a type that implements error, or to any interface
+// type. As returns false if err is nil.
+func As(err error, target interface{}) bool {
+	if target == nil {
+		panic("errors: target cannot be nil")
+	}
+	val := reflect.ValueOf(target)
+	typ := val.Type()
+	if typ.Kind() != reflect.Ptr || val.IsNil() {
+		panic("errors: target must be a non-nil pointer")
+	}
+	if e := typ.Elem(); e.Kind() != reflect.Interface && !e.Implements(reflect.TypeOf((*error)(nil)).Elem()) {
+		panic("errors: *target must be interface or implement error")
+	}
+	targetType := typ.Elem()
+	for err != nil {
+		if reflect.TypeOf(err).AssignableTo(targetType) {
+			val.Elem().Set(reflect.ValueOf(err))
+			return true
+		}
+		if x, ok := err.(interface{ As(interface{}) bool }); ok && x.As(target) {
+			return true
+		}
+		err = Unwrap(err)
+	}
+	return false
+}
+
+// Cause returns the root cause of the error, which is defined as the first error in the chain. The original
+// error is returned if it does not implement `Unwrap() error` and nil is returned if the error is nil.
+func Cause(err error) error {
+	for {
+		uerr := Unwrap(err)
+		if uerr == nil {
+			return err
+		}
+		err = uerr
+	}
+}
+
 type rootError struct {
 	msg   string
 	stack *stack
