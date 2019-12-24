dff2e5956ee1697728ea87169377e60541ded093
Chris Harless <53271741+morningvera@users.noreply.github.com>
2019-12-24 14:23:05 -0500

    chore: improve docs and build/CI

--- Makefile
+++ Makefile
@@ -1,17 +1,63 @@
-PKGS = $(shell go list ./... | grep -v vendor)
-SRCDIRS := $(shell go list -f '{{.Dir}}' $(PKGS))
+.DEFAULT_GOAL       := help
+VERSION             := v0.0.0
+TARGET_MAX_CHAR_NUM := 20
 
-check: fmt lint test
+GREEN  := $(shell tput -Txterm setaf 2)
+YELLOW := $(shell tput -Txterm setaf 3)
+WHITE  := $(shell tput -Txterm setaf 7)
+RESET  := $(shell tput -Txterm sgr0)
 
+.PHONY: help build fmt lint test release-tag release-push
+
+## Show help
+help:
+	@echo 'Package eris provides a better way to handle errors in Go.'
+	@echo ''
+	@echo 'Usage:'
+	@echo '  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}'
+	@echo ''
+	@echo 'Targets:'
+	@awk '/^[a-zA-Z\-\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			printf "  ${YELLOW}%-$(TARGET_MAX_CHAR_NUM)s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
+
+## Build the code
+build:
+	@echo Building
+	@go build -v .
+
+## Format with go-fmt
 fmt:
-	@echo Running gofmt
-	@go fmt $(SRCDIRS)
+	@echo Formatting
+	@go fmt .
 
+## Lint with golangci-lint
 lint:
-	@echo Running golangci linter
+	@echo Linting
 	@go get github.com/golangci/golangci-lint
 	@golangci-lint run ./... --deadline 5m
 
+## Run the tests
 test:
 	@echo Running tests
-	@go test -race $(PKGS)
+	@go test -race -v .
+
+## Stage a release (usage: make release-tag VERSION={VERSION_TAG})
+release-tag: build fmt lint test
+	@echo Tagging release with version "${VERSION}"
+	@git tag -a ${VERSION} -m "chore: release version '${VERSION}'"
+	@echo Generating changelog
+	@git-chglog -o CHANGELOG.md
+	@git add CHANGELOG.md
+	@git commit -m "chore: update changelog for version '${VERSION}'"
+
+## Push a release (warning: make sure the release was staged properly before doing this)
+release-push:
+	@echo Publishing release
+	@git push --follow-tags
--- eris.go
+++ eris.go
@@ -1,5 +1,54 @@
-// todo: copy parts of readme text here once the examples are done
-//       also want to go more in depth on certain things
+// Package eris provides a better way to handle errors in Go.
+//
+// Types of errors
+//
+// `eris` is concerned with only three different types of errors: root errors,
+// wrap errors, and external errors. Root and wrap errors are defined types in
+// this package and all other error types are external or third-party errors.
+//
+// Root errors are created via `eris.New` and `eris.Errorf`. Generally, it's a
+// good idea to maintain a set of root errors that are then wrapped with
+// additional context whenever an error of that type occurs. Wrap errors
+// represent a stack of errors that have been wrapped with additional context.
+// Unwrapping these errors via `eris.Unwrap` will return the next error in the
+// stack until a root error is reached. `eris.Cause` will also retrieve the root
+// error.
+//
+// When external error types are wrapped with additional context, a root error
+// is first created from the original error. This creates a stack trace for the
+// error and allows it to function with the rest of the `eris` package.
+//
+// Wrapping errors with additional context
+//
+// `eris.Wrap` adds context to an error while preserving the type of the
+// original error. This method behaves differently for each error type. For
+// root errors, the stack trace is reset to the current callers which ensures
+// traces are correct when using global/sentinel error values. Wrapped error
+// types are simply wrapped with the new context. For external types (i.e.
+// something other than root or wrap errors), a new root error is created for
+// the original error and then it's wrapped with the additional context.
+//
+// 		_, err := db.Get(id)
+// 		if err != nil {
+// 			return eris.Wrapf(err, "error getting resource '%v'", id)
+// 		}
+//
+// Inspecting error types
+//
+// The `eris` package provides a few ways to inspect and compare error types.
+// `eris.Is` returns true if a particular error appears anywhere in the error
+// chain, and `eris.Cause` returns the root cause of the error. Currently,
+// `eris.Is` works simply by comparing error messages with each other. If an
+// error contains a particular error message anywhere in its chain (e.g. "not
+// found"), it's defined to be that error type (i.e. `eris.Is` will return
+// `true`).
+//
+// 		NotFound := eris.New("not found")
+// 		_, err := db.Get(id)
+// 		if eris.Is(err, NotFound) || eris.Cause(err) == NotFound {
+//			return eris.Wrapf(err, "resource '%v' not found", id)
+//		}
+//
 package eris
 
 import (
